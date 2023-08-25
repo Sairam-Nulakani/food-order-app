20419a6815f6667dd68516e3838f6244c386a066
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-08-25 15:10:16 +0530

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
       <div className="hidden md:flex gap-4 flex-1 font-bold">
-        <Link href="/">Homepage</Link>
+        <Link href="/">Home</Link>
         <Link href="/menu">Menu</Link>
         <Link href="/">Contact</Link>
       </div>
